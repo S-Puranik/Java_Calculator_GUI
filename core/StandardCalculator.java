a7c728782f2b4a5130efeb8ea452a77d37547edb
AdvikShr <advikshrivastava47@gmail.com>
2026-04-14 20:52:52 +0530

    Create StandardCalculator.java

---
+++ core/StandardCalculator.java
@@ -0,0 +1,33 @@
+package calculator.core;
+
+public class StandardCalculator {
+
+    private double firstNumber;
+    private String operator;
+
+    public void setFirstNumber(double num) {
+        this.firstNumber = num;
+    }
+
+    public void setOperator(String op) {
+        this.operator = op;
+    }
+
+    public double calculate(double secondNumber) {
+        switch (operator) {
+            case "+":
+                return firstNumber + secondNumber;
+            case "-":
+                return firstNumber - secondNumber;
+            case "*":
+                return firstNumber * secondNumber;
+            case "/":
+                if (secondNumber == 0) {
+                    throw new ArithmeticException("Divide by zero");
+                }
+                return firstNumber / secondNumber;
+            default:
+                return 0;
+        }
+    }
+}
